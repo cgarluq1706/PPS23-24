f5519a6fb5f5de6bca2cbded99f53f1728cb97a7
cgarluq1706 <167620837+cgarluq1706@users.noreply.github.com>
2025-05-15 13:57:39 +0000

    Update conexion.js

--- src/conexion.js
+++ src/conexion.js
@@ -1,19 +1,18 @@
 const mysql = require('mysql');
+require('dotenv').config(); // Importa las variables desde .env
 
 // MySQL Connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'admin',
-    database: 'red_social',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL database');
 });
-
-
 
 module.exports = connection;
